2fdca9a0a281072df0c9cadf3e15f81f4b28f6c8
Soheib Kaddouri <souhaibkaddouri12@gmail.com>
2026-04-24 02:56:39 +0100

    Update ServerLauncher.java

--- src/main/java/com/smartcampus/server/ServerLauncher.java
+++ src/main/java/com/smartcampus/server/ServerLauncher.java
@@ -12,15 +12,21 @@ package com.smartcampus.server;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.jackson.JacksonFeature; // Add this import
 
 import java.net.URI;
 
 public class ServerLauncher {
 
     public static void main(String[] args) {
-        final String BASE_URI = "http://localhost:8080/";
+        // 1. Updated URI to include your api/v1 prefix
+        final String BASE_URI = "http://localhost:8080/api/v1/";
 
-        ResourceConfig config = new ResourceConfig().packages("com.smartcampus");
+        // 2. Register Jackson for JSON support
+        ResourceConfig config = new ResourceConfig()
+                .packages("com.smartcampus")
+                .register(JacksonFeature.class); 
+
         HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
 
         System.out.println("SmartCampus API running at " + BASE_URI);
